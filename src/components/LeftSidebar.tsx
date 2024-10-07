928a9e2776364e3e85526aad003eca556d5816a7
My EVM <yo@myevm.org>
2024-10-07 07:25:29 +0000

    e

--- src/components/LeftSidebar.tsx
+++ src/components/LeftSidebar.tsx
@@ -8,15 +8,15 @@ const LeftSidebar = () => {
         <div className="liquid">
           <svg className="water" viewBox="0 0 200 100">
             <defs>
-              <linearGradient id="waterGradient" x1="0%" y1="0%" x2="0%" y2="100%">
-                <stop offset="0" style={{ stopColor: '#C4FFBA' }} />
-                <stop offset="0.1643" style={{ stopColor: '#9FE194' }} />
-                <stop offset="0.3574" style={{ stopColor: '#A2E897' }} />
-                <stop offset="0.5431" style={{ stopColor: '#4D9441' }} />
-                <stop offset="0.7168" style={{ stopColor: '#53864B' }} />
-                <stop offset="0.874" style={{ stopColor: '#4D9441' }} />
-                <stop offset="1" style={{ stopColor: '#4D9441' }} />
-              </linearGradient>
+            <linearGradient id="waterGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0" style={{ stopColor: '#FFBABA' }} />  {/* Light Red */}
+              <stop offset="0.1643" style={{ stopColor: '#FF7F7F' }} />  {/* Medium Light Red */}
+              <stop offset="0.3574" style={{ stopColor: '#FF4D4D' }} />  {/* Medium Red */}
+              <stop offset="0.5431" style={{ stopColor: '#E60000' }} />  {/* Dark Red */}
+              <stop offset="0.7168" style={{ stopColor: '#B30000' }} />  {/* Darker Red */}
+              <stop offset="0.874" style={{ stopColor: '#E60000' }} />  {/* Dark Red */}
+              <stop offset="1" style={{ stopColor: '#B30000' }} />  {/* Darker Red */}
+            </linearGradient>
             </defs>
             <path fill="url(#waterGradient)" d="
               M 0,0 v 100 h 200 v -100 
